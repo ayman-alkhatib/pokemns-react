0cca57b63cd83e54b9206ddbda604b5a3e627909
Ayman ALKHATIB <aemn22384@gmail.com>
2024-11-27 12:07:40 +0100

    create app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import fetchPokemons from "./logic/fetchPokemons.js"
+import PokemonsListPage from "./pages/PokemonsListPage.jsx";
+import PokemonDetailsPage from "./pages/PokemonDetailsPage.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Link,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <Link to="/">home</Link>
+      <Link to="/pokemon">pokemonPage</Link>
+      <Link to="/details">details</Link>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index path="/pokemon" element={<PokemonsListPage />} loader={fetchPokemons} />
+      <Route path="/details" element={<PokemonDetailsPage />} />
+    </Route>
+  )
+);
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hello react
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
