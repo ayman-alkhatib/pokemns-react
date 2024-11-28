4afd796e793999264a413d0a35a092ba9490cc6d
Ayman ALKHATIB <aemn22384@gmail.com>
2024-11-28 10:14:30 +0100

    delete context and use globale object to pass the pokemon name

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import { RouterProvider, } from "react-router-dom";
-import { useContext } from "react";
-import ContextProvider, { GlobalContext } from "./ContextProvider.jsx";
 import { router } from "./router.js";
 
 
 
 function App() {
-  const [pokemonName, setPokemonName] = useContext(GlobalContext);
   return (
     <div className="App">
-      <ContextProvider >
-        <RouterProvider router={router} />
-      </ContextProvider>
+      <RouterProvider router={router} />
     </div>
   );
 }
