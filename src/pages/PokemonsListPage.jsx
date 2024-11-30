6e1e8ebb4dd2a5d09e90729fbed5283da7e7dc8a
Ayman ALKHATIB <aemn22384@gmail.com>
2024-11-30 14:44:57 +0100

    pass onclick funciton to pokemonCard

--- src/pages/PokemonsListPage.jsx
+++ src/pages/PokemonsListPage.jsx
@@ -1,20 +1,23 @@
-import { useLoaderData, useNavigation } from "react-router-dom";
+import { useLoaderData, useNavigate, useNavigation } from "react-router-dom";
 import PokemonCard from "../components/PokemonCard.jsx";
 
 function PokemonsListPage() {
   const data = useLoaderData();
   const navigation = useNavigation();
+  const navigate = useNavigate();
 
   if (navigation.state === "loading") return <h1>Loading!</h1>;
-
   return (
     <div className="pokemon-list">
       {data.map((pokemon) => {
         return (
           <PokemonCard
-            name={pokemon?.name}
-            img={pokemon?.img}
-            key={pokemon?.name}
+            clickFn={(name) => {
+              navigate(`/details/${pokemon.name}`);
+            }}
+            name={pokemon.name}
+            img={pokemon.img}
+            key={pokemon.name}
           />
         );
       })}
