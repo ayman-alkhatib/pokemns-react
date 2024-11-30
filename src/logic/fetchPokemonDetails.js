4935006704c006caa82eccfb782884de01a36de8
Ayman ALKHATIB <aemn22384@gmail.com>
2024-11-30 14:42:58 +0100

    update fetchPokemonDetails and fetch more details (HP,Attack,Defense...)

--- src/logic/fetchPokemonDetails.js
+++ src/logic/fetchPokemonDetails.js
@@ -1,18 +1,20 @@
 export default async function fetchPokemonDetails(name) {
-
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     const jsonResult = await res.json()
-
     const pokemon = {
         name: jsonResult.name,
-        img: jsonResult.sprites.front_shiny,
+        img: jsonResult.sprites.other.dream_world.front_default,
         height: jsonResult.height,
         weight: jsonResult.weight,
         exp: jsonResult.base_experience,
-        hp: jsonResult.stats.at(0).base_stat,
-        attack: jsonResult.stats.at(1).base_stat,
-        defense: jsonResult.stats.at(2).base_stat,
-        speed: jsonResult.stats.at(5).base_stat,
+        type: jsonResult.types.at(0).type.name,
+        stats: [
+            { statName: "HP", baseStat: jsonResult.stats.at(0).base_stat, maxValue: 100 },
+            { statName: "Attack", baseStat: jsonResult.stats.at(1).base_stat, maxValue: 100 },
+            { statName: "Defense", baseStat: jsonResult.stats.at(2).base_stat, maxValue: 100 },
+            { statName: "Speed", baseStat: jsonResult.stats.at(5).base_stat, maxValue: 100 },
+            { statName: "Exp", baseStat: jsonResult.base_experience, maxValue: 300 },
+        ]
     }
     return pokemon
 }
