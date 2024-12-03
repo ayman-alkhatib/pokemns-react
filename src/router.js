e6a8811e38453e149ad4bb6562ab0e22d16b6b46
Ayman ALKHATIB <aemn22384@gmail.com>
2024-12-03 14:10:06 +0100

    delete the / route

--- src/router.js
+++ src/router.js
@@ -8,16 +8,15 @@ import fetchPokemons from "./logic/fetchPokemons";
 import fetchPokemonDetails from "./logic/fetchPokemonDetails";
 import fetchComparisonData from "./logic/fetchComparisonData"
 
-export const routes = { pokemons: "pokemons", details: "details/", comparison: "comparison/" }
+export const routes = { pokemons: "/", details: "details/", comparison: "comparison/" }
 
 export const router = createBrowserRouter(
 
 
     createRoutesFromElements(
         <Route path="/" element={<Nav />} errorElement={<ErrorMessage message={"test"} />}>
-            <Route path="pokemons" element={<PokemonsListPage />} loader={fetchPokemons} />
+            <Route path="/" element={<PokemonsListPage />} loader={fetchPokemons} />
             <Route path="details/:name" element={<PokemonDetailsPage />} loader={({ params }) => fetchPokemonDetails(params.name)} />
-
             <Route path="comparison/:name1/:name2" element={<PokemonsComprisonPage />} loader={({ params }) => fetchComparisonData(params.name1, params.name2)}
             />
         </Route>
